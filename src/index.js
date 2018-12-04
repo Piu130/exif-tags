a53aecd2b166b558bdd7c1c13fdbbc2834c89fc8
Ramon Boss <ramon.boss@outlook.com>
2018-12-04 10:17:48 +0100

    ordered by value

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * Indicates start of a JPEG.
  * @type {number}
  */
-export const jpegStartNumber = 0xFFD8
+export const jpegStartNumber = 0xffd8
 
 /**
  * Hex value for 'Exif'.
@@ -14,13 +14,13 @@ export const exifString = 0x45786966 // Exif
  * Indicates start of the EXIF attribute.
  * @type {number}
  */
-export const exifStartNumber = 0xFFE1
+export const exifStartNumber = 0xffe1
 
 /**
  * Hex value for 'MM'. This indicates big endian.
  * @type {number}
  */
-export const bigEndianIndicator = 0x4D4D // MM
+export const bigEndianIndicator = 0x4d4d // MM
 
 /**
  * Hex value for 'II'. This indicates little endian.
@@ -33,27 +33,8 @@ export const littleEndianIndicator = 0x4949 // II
  * @type {Object.<number, string>}
  */
 export const exifIfdTags = {
-  0x9000: 'ExifVersion',
-  0xA000: 'FlashpixVersion',
-  0xA001: 'ColorSpace',
-  0xA500: 'Gamma',
-  0x9101: 'ComponentsConfiguration',
-  0x9102: 'CompressedBitsPerPixel',
-  0xA002: 'PixelXDimension',
-  0xA003: 'PixelYDimension',
-  0x927C: 'MakerNote',
-  0x9286: 'UserComment',
-  0xA004: 'RelatedSoundFile',
-  0x9003: 'DateTimeOriginal',
-  0x9004: 'DateTimeDigitized',
-  0x9010: 'Time',
-  0x9011: 'OffsetTimeOriginal',
-  0x9012: 'OffsetTimeDigitized',
-  0x9290: 'SubSecTime',
-  0x9291: 'SubSecTimeOriginal',
-  0x9292: 'SubSecTimeDigitized',
-  0x829A: 'ExposureTime',
-  0x829D: 'FNumber',
+  0x829a: 'ExposureTime',
+  0x829d: 'FNumber',
   0x8822: 'ExposureProgram',
   0x8824: 'SpectralSensitivity',
   0x8827: 'PhotographicSensitivity',
@@ -64,6 +45,14 @@ export const exifIfdTags = {
   0x8833: 'ISOSpeed',
   0x8834: 'ISOSpeedLatitudeyyy',
   0x8835: 'ISOSpeedLatitudezzz',
+  0x9000: 'ExifVersion',
+  0x9003: 'DateTimeOriginal',
+  0x9004: 'DateTimeDigitized',
+  0x9010: 'Time',
+  0x9011: 'OffsetTimeOriginal',
+  0x9012: 'OffsetTimeDigitized',
+  0x9101: 'ComponentsConfiguration',
+  0x9102: 'CompressedBitsPerPixel',
   0x9201: 'ShutterSpeedValue',
   0x9202: 'ApertureValue',
   0x9203: 'BrightnessValue',
@@ -73,45 +62,56 @@ export const exifIfdTags = {
   0x9207: 'MeteringMode',
   0x9208: 'LightSource',
   0x9209: 'Flash',
-  0x920A: 'FocalLength',
+  0x920a: 'FocalLength',
   0x9214: 'SubjectArea',
-  0xA20B: 'FlashEnergy',
-  0xA20C: 'SpatialFrequencyResponse',
-  0xA20E: 'FocalPlaneXResolution',
-  0xA20F: 'FocalPlaneYResolution',
-  0xA210: 'FocalPlaneResolutionUnit',
-  0xA214: 'SubjectLocation',
-  0xA215: 'ExposureIndex',
-  0xA217: 'SensingMethod',
-  0xA300: 'FileSource',
-  0xA301: 'SceneType',
-  0xA302: 'CFAPattern',
-  0xA401: 'CustomRendered',
-  0xA402: 'ExposureMode',
-  0xA403: 'WhiteBalance',
-  0xA404: 'DigitalZoomRatio',
-  0xA405: 'FocalLengthIn35mmFilm',
-  0xA406: 'SceneCaptureType',
-  0xA407: 'GainControl',
-  0xA408: 'Contrast',
-  0xA409: 'Saturation',
-  0xA40A: 'Sharpness',
-  0xA40B: 'DeviceSettingDescription',
-  0xA40C: 'SubjectDistanceRange',
+  0x927c: 'MakerNote',
+  0x9286: 'UserComment',
+  0x9290: 'SubSecTime',
+  0x9291: 'SubSecTimeOriginal',
+  0x9292: 'SubSecTimeDigitized',
   0x9400: 'Temperature',
   0x9401: 'Humidity',
   0x9402: 'Pressure',
   0x9403: 'WaterDepth',
   0x9404: 'Acceleration',
   0x9405: 'CameraElevationAngle',
-  0xA420: 'ImageUniqueID',
-  0xA430: 'CameraOwnerName',
-  0xA431: 'BodySerialNumber',
-  0xA432: 'LensSpecification',
-  0xA433: 'LensMake',
-  0xA434: 'LensModel',
-  0xA435: 'LensSerialNumber',
-  0xEA1D: 'OffsetSchema'
+  0xa000: 'FlashpixVersion',
+  0xa001: 'ColorSpace',
+  0xa002: 'PixelXDimension',
+  0xa003: 'PixelYDimension',
+  0xa004: 'RelatedSoundFile',
+  0xa20b: 'FlashEnergy',
+  0xa20c: 'SpatialFrequencyResponse',
+  0xa20e: 'FocalPlaneXResolution',
+  0xa20f: 'FocalPlaneYResolution',
+  0xa210: 'FocalPlaneResolutionUnit',
+  0xa214: 'SubjectLocation',
+  0xa215: 'ExposureIndex',
+  0xa217: 'SensingMethod',
+  0xa300: 'FileSource',
+  0xa301: 'SceneType',
+  0xa302: 'CFAPattern',
+  0xa401: 'CustomRendered',
+  0xa402: 'ExposureMode',
+  0xa403: 'WhiteBalance',
+  0xa404: 'DigitalZoomRatio',
+  0xa405: 'FocalLengthIn35mmFilm',
+  0xa406: 'SceneCaptureType',
+  0xa407: 'GainControl',
+  0xa408: 'Contrast',
+  0xa409: 'Saturation',
+  0xa40a: 'Sharpness',
+  0xa40b: 'DeviceSettingDescription',
+  0xa40c: 'SubjectDistanceRange',
+  0xa420: 'ImageUniqueID',
+  0xa430: 'CameraOwnerName',
+  0xa431: 'BodySerialNumber',
+  0xa432: 'LensSpecification',
+  0xa433: 'LensMake',
+  0xa434: 'LensModel',
+  0xa435: 'LensSerialNumber',
+  0xa500: 'Gamma',
+  0xea1d: 'OffsetSchema'
 }
 
 /**
@@ -121,7 +121,7 @@ export const exifIfdTags = {
 export const exifPointerTags = {
   0x8769: 'ExifIFDPointer',
   0x8825: 'GPSInfoIFDPointer',
-  0xA005: 'InteroperabilityIFDPointer'
+  0xa005: 'InteroperabilityIFDPointer'
 }
 
 /**
@@ -139,12 +139,12 @@ export const gpsTags = {
   0x0007: 'GPSTimeStamp',
   0x0008: 'GPSSatellites',
   0x0009: 'GPSStatus',
-  0x000A: 'GPSMeasureMode',
-  0x000B: 'GPSDOP',
-  0x000C: 'GPSSpeedRef',
-  0x000D: 'GPSSpeed ',
-  0x000E: 'GPSTrackRef',
-  0x000F: 'GPSTrack',
+  0x000a: 'GPSMeasureMode',
+  0x000b: 'GPSDOP',
+  0x000c: 'GPSSpeedRef',
+  0x000d: 'GPSSpeed ',
+  0x000e: 'GPSTrackRef',
+  0x000f: 'GPSTrack',
   0x0010: 'GPSImgDirectionRef',
   0x0011: 'GPSImgDirection',
   0x0012: 'GPSMapDatum',
@@ -155,12 +155,12 @@ export const gpsTags = {
   0x0017: 'GPSDestBearingRef',
   0x0018: 'GPSDestBearing',
   0x0019: 'GPSDestDistanceRef',
-  0x001A: 'GPSDestDistance',
-  0x001B: 'GPSProcessingMethod',
-  0x001C: 'GPSAreaInformation',
-  0x001D: 'GPSDateStamp',
-  0x001E: 'GPSDifferential',
-  0x001F: 'GPSHPositioningError'
+  0x001a: 'GPSDestDistance',
+  0x001b: 'GPSProcessingMethod',
+  0x001c: 'GPSAreaInformation',
+  0x001d: 'GPSDateStamp',
+  0x001e: 'GPSDifferential',
+  0x001f: 'GPSHPositioningError'
 }
 
 /**
@@ -173,30 +173,30 @@ export const tiffTags = {
   0x0102: 'BitsPerSample',
   0x0103: 'Compression',
   0x0106: 'PhotometricInterpretation',
+  0x010e: 'ImageDescription',
+  0x010f: 'Make',
+  0x0110: 'Model',
+  0x0111: 'StripOffsets',
   0x0112: 'Orientation',
   0x0115: 'SamplesPerPixel',
-  0x011C: 'PlanarConfiguration',
-  0x0212: 'YCbCrSubSampling',
-  0x0213: 'YCbCrPositioning',
-  0x011A: 'XResolution',
-  0x011B: 'YResolution',
-  0x0128: 'ResolutionUnit',
-  0x0111: 'StripOffsets',
   0x0116: 'RowsPerStrip',
   0x0117: 'StripByteCounts',
+  0x011a: 'XResolution',
+  0x011b: 'YResolution',
+  0x011c: 'PlanarConfiguration',
+  0x0128: 'ResolutionUnit',
+  0x012d: 'TransferFunction',
+  0x0131: 'Software',
+  0x0132: 'DateTime',
+  0x013b: 'Artist',
+  0x013e: 'WhitePoint',
+  0x013f: 'PrimaryChromaticities',
   0x0201: 'JPEGInterchangeFormat',
   0x0202: 'JPEGInterchangeFormatLength',
-  0x012D: 'TransferFunction',
-  0x013E: 'WhitePoint',
-  0x013F: 'PrimaryChromaticities',
   0x0211: 'YCbCrCoefficients',
+  0x0212: 'YCbCrSubSampling',
+  0x0213: 'YCbCrPositioning',
   0x0214: 'ReferenceBlackWhite',
-  0x0132: 'DateTime',
-  0x010E: 'ImageDescription',
-  0x010F: 'Make',
-  0x0110: 'Model',
-  0x0131: 'Software',
-  0x013B: 'Artist',
   0x8298: 'Copyright'
 }
 
